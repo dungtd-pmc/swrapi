2bbe358512fa8ad115a06b58a786afd4dc9f5606
Dung Tran <dung.td@pharmacity.vn>
2022-12-25 17:49:25 +0700

    fix missing type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as ApiConfig } from './ApiConfig'
 export { default as useApi } from './useApi'
-export type { ApiConfiguration } from './types'
+export type { Api, ApiConfiguration } from './types'
